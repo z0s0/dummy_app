44dd2d380ab867db191f38024f2827d1e7f0b85d
vladislav <vlad.kovalenko@aviasales.ru>
2020-08-10 03:12:20 +0700

    extended greeting service

--- src/main/scala/ru/otus/sc/greet/dao/GreetingDao.scala
+++ src/main/scala/ru/otus/sc/greet/dao/GreetingDao.scala
@@ -3,4 +3,5 @@ package ru.otus.sc.greet.dao
 trait GreetingDao {
   def greetingPrefix: String
   def greetingPostfix: String
+  def isKnownPerson(name: String): Boolean
 }
--- src/main/scala/ru/otus/sc/greet/service/impl/GreetingServiceImpl.scala
+++ src/main/scala/ru/otus/sc/greet/service/impl/GreetingServiceImpl.scala
@@ -5,8 +5,21 @@ import ru.otus.sc.greet.model.{GreetRequest, GreetResponse}
 import ru.otus.sc.greet.service.GreetingService
 
 class GreetingServiceImpl(dao: GreetingDao) extends GreetingService {
+  var humansGreetedCounter = 0
+
   def greet(request: GreetRequest): GreetResponse =
-    if (request.isHuman)
-      GreetResponse(s"${dao.greetingPrefix} ${request.name} ${dao.greetingPostfix}")
-    else GreetResponse("AAAAAAAAAA!!!!!!")
+    if (request.isHuman) {
+      incHumansGreetedCounter()
+      GreetResponse(
+        s"${dao.greetingPrefix} ${request.name} ${dao.greetingPostfix}. You are ${humansGreetedCounter}th human found"
+      )
+    } else GreetResponse("AAAAAAAAAA!!!!!!")
+
+  def echo(request: GreetRequest): GreetRequest = request
+
+  def humansGreeted(): Int = humansGreetedCounter
+
+  def isKnownPerson(request: GreetRequest): Boolean = dao.isKnownPerson(request.name)
+
+  private def incHumansGreetedCounter(): Unit = humansGreetedCounter += 1
 }
